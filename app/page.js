8143dd44df1464e63876a0d49d7271173e121e39
Zechst <spiderspy_1000@hotmail.com>
2024-12-11 16:28:07 +0800

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+"use client"; // Add this line at the top
+
+import Head from "next/head";
+import Init from "./components/init";
+import { useEffect } from "react";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Swyber Town Demo</title>
+      </Head>
+    </>
+  );
+}
