03853999aa48b7c15fdc1644ec0426713e2eefe2
Azrio <38596941+Zechst@users.noreply.github.com>
2024-12-13 01:39:14 +0800

    wip: game connection

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 import { auth, database } from "./config/firebase";
 import { onAuthStateChanged, signInAnonymously } from "firebase/auth";
-import { ref, set, onDisconnect } from "firebase/database";
+import { ref, set, onDisconnect, on } from "firebase/database";
 
 //Misc Helpers
 
@@ -67,6 +67,24 @@ export const initializeFirebaseAuth = () => {
   let playerRef;
   let playerData;
 
+  function initGame() {
+    const allPlayersRef = ref(database, `players`);
+    const allCoinsRef = ref(database, `coins`);
+
+    allPlayersRef.on(
+      "value",
+      (snapshot = {
+        //Fires whenever a change occurs
+      })
+    );
+    allPlayersRef.on("child_added", (snapshot) => {
+      //Fire whenever a new node is added to the tree
+      const addedPlayer = snapshot.val();
+      const characterElement = document.createElement("div");
+      characterElement.classList.add("Character", "grid-cell");
+    });
+  }
+
   onAuthStateChanged(auth, (user) => {
     console.log(user);
     if (user) {
@@ -87,15 +105,11 @@ export const initializeFirebaseAuth = () => {
 
       //Remove me from Firebase when I disconnect
       onDisconnect(playerRef).remove();
+
+      //Begin the game now that we are signed in
+      initGame();
     } else {
-      console.log("No user logged in. Attempting anonymous sign-in...");
-      signInAnonymously(auth);
-      // .then((userCredential) => {
-      // console.log("Signed in anonymously:", userCredential.user.uid);
-      // })
-      // .catch((error) => {
-      //   console.error("Authentication Error:", error.code, error.message);
-      // });
+      //You're logged out.
     }
   });
 };
