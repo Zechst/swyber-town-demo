c65c4dfb19315e8ff980e13a064d2c29095003da
Zechst <spiderspy_1000@hotmail.com>
2024-12-12 16:50:07 +0800

    feat: added firebase

---
+++ app/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRvzSq0KFBHug66EZjKL74c5lbRm8w-9U",
+  authDomain: "swyber-town-demo.firebaseapp.com",
+  databaseURL:
+    "https://swyber-town-demo-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "swyber-town-demo",
+  storageBucket: "swyber-town-demo.firebasestorage.app",
+  messagingSenderId: "349926698372",
+  appId: "1:349926698372:web:d31d1464a6ee045f31c1d1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
